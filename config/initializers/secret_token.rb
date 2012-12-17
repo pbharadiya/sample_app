e6e2ed7909f11d7da7c329e0d8faf5fc68f2db75
parth <parthb@parthm.cybage.com>
2012-12-17 17:29:37 +0530

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '1b95b6475b928b393b693a3026f093e048aa26002eed60dac66261ded70c5229e40c754afab5eafb30c4a7de82bc6329bd9c2e9584b2bd6f17089ed139c87494'
