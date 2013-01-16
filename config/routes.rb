d2e975a620598c74c69c378e6482693eae50c21f
parth <exit>
2013-01-16 15:45:50 +0530

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
-
+  resources :users
+  
+  #get "users/new"
+  match '/signup', to: 'users#new'
   #get "static_pages/home"
   root to: 'static_pages#home'
  # match '/home', to: 'static_pages#home'
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name "Parth Bharadiya"
+    email "parth@parth.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
